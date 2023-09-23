ce2e3721da66ed3592fb443e12bc0dd48647e1bd
saif-rehma <144496587+saif-rehma@users.noreply.github.com>
2023-09-23 16:28:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+// impoting Guest List from exercise 15
+const exercise_15_1 = require("../exercise 15");
+console.log(`\nPrinting message indicating number of guest`);
+console.log(`We had finally invited ${exercise_15_1.guestList.length} guest in exercise 15.`);
---
+++ index.ts
@@ -0,0 +1,14 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+// impoting Guest List from exercise 15
+
+import{ guestList } from '../exercise 15'
+
+
+console.log(`\nPrinting message indicating number of guest`)
+console.log(`We had finally invited ${guestList.length} guest in exercise 15.`)
+
+
+
+
+
